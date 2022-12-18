e970dd5ca77f4dc0e7137bf9e70fdfd25e861e70
ACMenstab <119356920+ACMenstab@users.noreply.github.com>
2022-12-18 20:48:25 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n,i,x;
+   cin>>n;
+   for(i=0 ;i<n;i++)
+   {cin>>x;
+   if(x==1)
+   {cout<<"HARD";return 0;}
+   }
+   cout<<"EASY";
+  
+}
+
